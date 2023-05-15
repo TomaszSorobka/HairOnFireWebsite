80311120a46edeb01a181fbc2b0a42db36c95515
TomaszSorobka <tomek.sorobka@gmail.com>
2023-05-15 22:13:48 +0200

    fixed apostrophes

--- pages/about.js
+++ pages/about.js
@@ -6,9 +6,9 @@ const Home = ({problems}) => (
       <div className="aboutme-heading">
         <h1>What is it about?</h1>
         <br></br><br></br>
-        <p>Welcome to Hair On Fire, an online repository of students’ problems sourced from various forums, that serves as an inspiration for entrepreneurial students looking to start their own business or side-hustle.
+        <p>Welcome to Hair On Fire, an online repository of students&apos; problems sourced from various forums, that serves as an inspiration for entrepreneurial students looking to start their own business or side-hustle.
 <br></br><br></br>
-We understand that finding a valid business idea can be challenging, especially for students who lack significant industry experience. That's why we've created this platform to provide a solution to this problem.
+We understand that finding a valid business idea can be challenging, especially for students who lack significant industry experience. That&apos;s why we&apos;ve created this platform to provide a solution to this problem.
 <br></br><br></br>
 Our website is designed for entrepreneurial students who are looking for inspiration on what actual and real problems they can solve and potentially turn into a successful business. We provide a comprehensive list of problems, sourced from various student forums, that are relevant to our users.
 <br></br><br></br>
